8ccf5d8889dd3463f71709758e3e32335f146035
LEA HADIDA <hadida.lea@gmail.com>
2023-03-23 22:52:43 +0100

    added error messages when empty fields

--- main.js
+++ main.js
@@ -132,28 +132,45 @@ navUl.addEventListener("click", () => {
 
 // MENU BURGER END
 
-// MENU BURGER END
 
-// BUBBLE POPS WHEN YOU CLICK ON SEND //
 const button = document.querySelector('.button');
 const popUp = document.querySelector('.bubble');
 const hedwigeMobile = document.querySelector('.hedwige-mobile');
 const hedwigeDesktop = document.querySelector('.hedwige-desktop');
 const close = document.querySelector('.close');
 
-// PERSONNALISATION OF THE POPUP //
-function senderName() {
-    let imput = document.getElementById('firstname').value;
-    document.getElementById('name').innerHTML = imput;
-}
 
-button.addEventListener('click', function () {
-    popUp.style.display = 'block';
-    hedwigeMobile.style.marginTop = '-5.5rem';
-    hedwigeDesktop.style.marginTop = '-78px';//-77px depending on the screen size :(
-    senderName();
+// BUBBLE POPS WHEN YOU CLICK ON SEND + ERROR MESSAGES WHEN EMPTY FIELDS //
+button.addEventListener('click', function (e) {
+    let userName = document.getElementById('firstname').value;
+    let userEmail = document.getElementById('email').value;
+    let userMessage = document.getElementById('message').value;
+    let messageErrorUser = document.getElementById('error-message-firstname');
+    let messageErrorEmail = document.getElementById('error-message-email');
+    let messageErrorText = document.getElementById('error-message-text');
+
+    if (userName === '') {
+        messageErrorUser.hidden = false;
+    }
+    if (userEmail === '') {
+        messageErrorEmail.hidden = false;
+    }
+    if (userMessage === '') {
+        messageErrorText.hidden = false;
+    }
+    else {
+        popUp.style.display = 'block';
+        popUp.style.transition = 'all 0.5s ease';
+        hedwigeMobile.style.marginTop = '-5.5rem';
+        hedwigeDesktop.style.marginTop = '-76px';//-78px -77px depending on the screen size :(
+        document.getElementById('name').innerHTML = userName;
+        messageErrorUser.hidden = true;
+        messageErrorEmail.hidden = true;
+        messageErrorText.hidden = true;
+    };
 });
 
+// BUBBLE ClOSES WHEN YOU CLICK ON THE CROSS//
 close.addEventListener('click', function () {
     popUp.style.display = 'none';
     // hedwigeMobile.style.marginTop = '-5.5rem';
